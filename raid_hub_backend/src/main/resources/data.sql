a7039cb9bce6ceded020257bea87e7100f20eabb
0verfl0w767 <0verfl0w767@naver.com>
2026-03-22 22:55:55 +0900

    chore: 초기 테스트 코드 삭제 (sql init test)

--- raid_hub_backend/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (username, password, role, enabled)
-VALUES ('admin', '$2a$10$YsvAvc7CUJQ1t8ybSjxvD.vCPjaKqREWnx9JFIYU0vsEEghZNElDO', 'ADMIN', true)
-ON CONFLICT (username) DO NOTHING;
-
